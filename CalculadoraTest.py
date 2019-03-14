01b89b0de2d9c96c4003fdd5ef6dc517db2c6778
Jose Fabian Laverde Triana <jf.laverde@uniandes.edu.co>
2019-03-13 21:54:19 -0500

    Paso 1-Se corrige el metodo sumar para que pase la prueba(verde)

--- CalculadoraTest.py
+++ CalculadoraTest.py
@@ -1,7 +1,11 @@
 from unittest import TestCase
 
-import Calculadora
+from Calculadora import Calculadora
 
 class CalculadoraTest(TestCase):
     def test_sumar(self):
         self.assertEqual(Calculadora().sumar(""),0,"Cadena vacia")
+
+
+
+
